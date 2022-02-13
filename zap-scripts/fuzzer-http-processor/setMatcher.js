39bb40ba49b8777d772010867bba7b3d07887052
HAHWUL <hahwul@gmail.com>
2022-02-14 01:07:11 +0900

    Create setMatcher.js

---
+++ zap-scripts/fuzzer-http-processor/setMatcher.js
@@ -0,0 +1,37 @@
+function getRequiredParamsNames(){
+	return [];
+}
+
+function getOptionalParamsNames(){
+	return ["statusCode","regex"];
+}
+
+function processMessage(utils, message) {
+	// Take no action
+}
+
+function processResult(utils, fuzzResult){
+	var params = utils.getParameters();
+	var fuzzed = fuzzResult.getHttpMessage();
+
+	if(params.regex != "") {
+		var matchPattern = new RegExp(params.regex);
+		var fuzzedResponseHeader = fuzzed.getResponseHeader().toString();
+		var fuzzedResponseBody = fuzzed.getResponseBody().toString();
+	  var regexHeaderFound = fuzzedResponseHeader.search(matchPattern) != -1;
+	  var regexBodyFound = fuzzedResponseBody.search(matchPattern) != -1;
+		
+    if(regexHeaderFound || regexBodyFound) {
+			return true;
+	  }
+	}
+
+	if(params.statusCode != "") {
+		var matchStatusCode = params.statusCode;
+		var fuzzedStatusCode = fuzzed.getResponseHeader().getStatusCode().toString();
+		if(fuzzedStatusCode == matchStatusCode) {
+			return true;
+	  }
+	}
+	return false;
+}
