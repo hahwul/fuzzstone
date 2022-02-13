f3509ee4982efec43eb862b57268c803957c7a34
HAHWUL <hahwul@gmail.com>
2022-02-13 13:58:57 +0900

    Update and rename cache-busting.js to addCacheBusting.js

---
+++ zap-scripts/fuzzer-http-processor/addCacheBusting.js
@@ -0,0 +1,43 @@
+// https://www.hahwul.com/2021/09/11/zap-fuzzer-and-scripting/#fuzz-with-cache-busting
+
+function processMessage(utils, message) {
+	var cbValue = "cbvalue"+Math.floor(Math.random() * 254)
+     var updatedUrlParams = setCacheBusting(message,cbValue);
+	message.getRequestHeader().setHeader("X-Cache-Busting", cbValue);
+}
+
+function setCacheBusting(message,cbValue) {
+    var URI = Java.type("org.apache.commons.httpclient.URI");
+    var params = message.getUrlParams();
+    var iterator = params.iterator();
+
+    if(iterator.hasNext()) {
+      var uri = message.getRequestHeader().getURI().toString() + "&cbvalue="+cbValue
+	 message.getRequestHeader().setURI(new URI(uri, false))
+    } else {
+      var uri = message.getRequestHeader().getURI().toString() + "?cbvalue="+cbValue
+	 message.getRequestHeader().setURI(new URI(uri, false))
+    }
+	
+
+
+    while(iterator.hasNext()) {
+        var param = iterator.next();
+        if (param.getName().equals("cachebusting")) {
+		param.setValue(cbValue)
+        }
+    }
+    return params;
+}
+
+function processResult(utils, fuzzResult){
+	return true;
+}
+
+function getRequiredParamsNames(){
+	return [];
+}
+
+function getOptionalParamsNames(){
+	return [];
+}
