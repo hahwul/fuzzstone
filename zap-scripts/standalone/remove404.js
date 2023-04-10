b1732bc00af82e11eefeb54ef76d1fae1c01e509
HAHWUL <hahwul@gmail.com>
2023-04-11 01:43:34 +0900

    Update remove404.js

--- zap-scripts/standalone/remove404.js
+++ zap-scripts/standalone/remove404.js
@@ -1,14 +1,13 @@
 function listChildren(node, level) {
     var j;
     for (j=0;j<node.getChildCount();j++) {
-        var code;
+    	var code;
         ref = node.getChildAt(j).getHistoryReference();
         if(ref.getStatusCode() == 404){
-          ref.getSiteNode().removeFromParent();
-	   } else {
-          listChildren(node.getChildAt(j), level+1);
-        }
-        
+        	ref.getSiteNode().removeFromParent();
+	} else {
+        	listChildren(node.getChildAt(j), level+1);
+	}
     }
 }
 
@@ -16,3 +15,5 @@ siteTree = model.getSession().getSiteTree()
 root = siteTree.getRoot();
 listChildren(root, 0);
 siteTree.reload();
+
+// http://www.hahwul.com/2023/04/11/clearing-404-from-zap-site-tree/
