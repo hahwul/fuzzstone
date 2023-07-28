e701eed3df82b0f63708f827f319b2bbe0c94206
HAHWUL <hahwul@gmail.com>
2023-07-28 18:22:45 +0900

    Create disableProxy.js

---
+++ zap-scripts/standalone/disableProxy.js
@@ -0,0 +1,19 @@
+var url = "http://localhost:8090/JSON/network/action/setHttpProxyEnabled/?enabled=false";
+
+var URL = Java.type('java.net.URL');
+var HttpURLConnection = Java.type('java.net.HttpURLConnection');
+
+try {
+    var connection = new URL(url).openConnection();
+    connection.setRequestMethod('GET');
+
+    var responseCode = connection.getResponseCode();
+    if (responseCode === 200) {
+        print("Enabled!");
+    } else {
+        print("Error: " + responseCode);
+    }
+
+} catch (e) {
+    print("Error: " + e);
+}
